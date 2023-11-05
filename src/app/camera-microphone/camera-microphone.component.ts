4ff2d9ec46a489a28141b6888f7e7b8a5b630084
Ghost <OverlyGenericAddress@pm.me>
2023-11-05 20:45:04 +0100

    Started implementing db to save recordings

--- src/app/camera-microphone/camera-microphone.component.ts
+++ src/app/camera-microphone/camera-microphone.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
+declare var window: any;  // New line to suppress TypeScript errors when accessing window.indexedDB
 interface MediaConstraints {
   [key: string]: boolean;
 }
@@ -12,7 +13,11 @@ interface MediaConstraints {
 export class CameraMicrophoneComponent {
   
   @ViewChild('videoElement') videoElement!: ElementRef;
-  @ViewChild('audioElement') audioElement!: ElementRef;
+  @ViewChild('recordedVideoElement') recordedVideoElement!: ElementRef;  // New ViewChild for the recorded video element
+  
+  mediaRecorder: MediaRecorder | null = null;  // New property to hold the MediaRecorder instance
+  recordedBlobs: Blob[] = [];  // New property to hold the recorded video data
+  db: IDBDatabase | null = null;  // New property to hold the IndexedDB database instance
 
   getUserMedia(constraints: MediaStreamConstraints): Promise<MediaStream> {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -31,18 +36,73 @@ export class CameraMicrophoneComponent {
     constraints[type] = true;
     this.getUserMedia(constraints as MediaStreamConstraints)
       .then(stream => {
-        const mediaControl = type === 'video' ? 
-                             this.videoElement.nativeElement : 
-                             this.audioElement.nativeElement;
+        const mediaControl = this.videoElement.nativeElement;
         if ('srcObject' in mediaControl) {
           mediaControl.srcObject = stream;
         } else {
           alert('Your browser does not support the srcObject property.');
         }
         mediaControl.play();
+        
+        this.mediaRecorder = new MediaRecorder(stream);  // Create a new MediaRecorder instance
+        this.mediaRecorder.ondataavailable = event => {  // Collect the recorded data
+          if (event.data && event.data.size > 0) {
+            this.recordedBlobs.push(event.data);
+          }
+        };
+        this.mediaRecorder.start();  // Start recording
       })
       .catch(err => {
         alert('Error: ' + err);
       });
   }
-}  
+  
+  stopRecording() {
+    if (this.mediaRecorder) {
+      this.mediaRecorder.stop();
+      this.mediaRecorder.onstop = () => {
+        const blob = new Blob(this.recordedBlobs, { type: 'video/webm' });  // Combine the recorded data into a single Blob
+        this.saveVideo(blob);  // Save the recorded video to IndexedDB
+      };
+    }
+  }
+
+  saveVideo(blob: Blob) {  // New method to save the recorded video to IndexedDB
+    const request = window.indexedDB.open('RecordedVideos', 1);
+    request.onupgradeneeded = (event: IDBVersionChangeEvent) => {
+      const db = (event.target as IDBOpenDBRequest).result;
+      db.createObjectStore('videos');
+    };
+    request.onsuccess = () => {
+      const db = request.result;
+      const transaction = db.transaction('videos', 'readwrite');
+      const videosStore = transaction.objectStore('videos');
+      videosStore.put(blob, 'video1');
+      transaction.oncomplete = () => {
+        this.loadVideo();  // Load the recorded video from IndexedDB
+      };
+    };
+    request.onerror = (event: Event) => {
+      console.error('Error opening IndexedDB:', event);
+    };
+  }
+
+  loadVideo() {  // New method to load the recorded video from IndexedDB
+    const request = window.indexedDB.open('RecordedVideos', 1);
+    request.onsuccess = () => {
+      const db = request.result;
+      const transaction = db.transaction('videos');
+      const videosStore = transaction.objectStore('videos');
+      const getRequest = videosStore.get('video1');
+      getRequest.onsuccess = () => {
+        const videoBlob = getRequest.result;
+        const videoUrl = URL.createObjectURL(videoBlob);
+        const mediaControl = this.recordedVideoElement.nativeElement;
+        mediaControl.src = videoUrl;
+      };
+    };
+    request.onerror = (event: Event) => {
+      console.error('Error opening IndexedDB:', event);
+    };
+  }
+}
