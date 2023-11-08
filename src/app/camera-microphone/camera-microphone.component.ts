3bd7868db5ae6d322a194b3078b4c6979c145954
Ghost <OverlyGenericAddress@pm.me>
2023-11-08 17:55:50 +0100

    Cleaned up the mess
    - Removed duplicate code
    - Removed nasty error supression
    - Added codecomments

--- src/app/camera-microphone/camera-microphone.component.ts
+++ src/app/camera-microphone/camera-microphone.component.ts
@@ -1,10 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
-declare var window: any;  // New line to suppress TypeScript errors when accessing window.indexedDB
-interface MediaConstraints {
-  [key: string]: boolean;
-}
 
+// Component used for recording video and audio from the user's camera and microphone
 @Component({
   selector: 'app-camera-microphone',
   templateUrl: './camera-microphone.component.html',
@@ -12,31 +9,39 @@ interface MediaConstraints {
 })
 export class CameraMicrophoneComponent implements OnInit {
   
+  // ViewChilds for recording & displying video elements
+  @ViewChild('recordedVideoElement') recordedVideoElement!: ElementRef; 
   @ViewChild('videoElement') videoElement!: ElementRef;
-  @ViewChild('recordedVideoElement') recordedVideoElement!: ElementRef;  // New ViewChild for the recorded video element
 
-  mediaRecorder: MediaRecorder | null = null;  // New property to hold the MediaRecorder instance
-  recordedBlobs: Blob[] = [];  // New property to hold the recorded video data
-  db: IDBDatabase | null = null;  // New property to hold the IndexedDB database instance
-  isRecording = false; // New property to track recording state
+  // Variable declarations
+  mediaRecorder: MediaRecorder | null = null;
+  recordedBlobs: Blob[] = [];
+  db: IDBDatabase | null = null;
+  isRecording = false;
 
+  // Constructor
   constructor() {
-    // The constructor should only be used to inject dependencies
   }
 
+  // OnInit method to load the video from IndexedDB by calling loadVideo on component initialization
   ngOnInit(): void {
-    this.loadVideo(); // Call loadVideo on component initialization
+    this.loadVideo();
   }
 
+  // Method to toggle recording by calling either stopRecording or getStream depending on the recording state variable
   toggleRecording() {
     if (this.isRecording) {
+      // Stop recording video and audio by calling stopRecording
       this.stopRecording();
     } else {
-      this.getStream({ video: true, audio: true }); // Start recording with video and audio
+       // Start recording video and audio by calling getStream with video and audio constraints
+      this.getStream({ video: true, audio: true });
     }
   }
   
+  // Method to get the user media
   getUserMedia(constraints: MediaStreamConstraints): Promise<MediaStream> {
+    // Check if navigator.mediaDevices.getUserMedia is supported and return a Promise if so
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       return navigator.mediaDevices.getUserMedia(constraints);
     } else {
@@ -44,97 +49,119 @@ export class CameraMicrophoneComponent implements OnInit {
     }
   }
 
+  // Method to get the recording stream and define the MediaRecorder instance to record it stream
   getStream(constraints: MediaStreamConstraints) {
+    
+    // Check if navigator.mediaDevices is supported and stop with an error if not
     if (!navigator.mediaDevices) {
       alert('User Media API not supported.');
       return;
     }
   
+    // Get the user media stream by calling getUserMedia
     this.getUserMedia(constraints)
       .then(stream => {
         const mediaControl = this.videoElement.nativeElement;
+        
+        // Check if the srcObject property is supported and set the video element's source to the stream if so
         if ('srcObject' in mediaControl) {
           mediaControl.srcObject = stream;
         } else {
           alert('Your browser does not support the srcObject property.');
         }
+        
+        // Activate the video element to display the stream to the user
         mediaControl.play();
   
-        // Setup for the MediaRecorder
+        // Set the MediaRecorder instance with the stream and options
         this.mediaRecorder = new MediaRecorder(stream, { mimeType: 'video/webm;codecs=vp9,opus' });
+        
+        // Set a MediaRecorder event handler to push the recorded data to the recordedBlobs array
         this.mediaRecorder.ondataavailable = event => {
           if (event.data && event.data.size > 0) {
             this.recordedBlobs.push(event.data);
           }
         };
+
+        // Set a MediaRecorder event handler to save the recorded video to IndexedDB when the recording stops
         this.mediaRecorder.onstop = () => {
-          // When recording stops, we create a new Blob from the data
+          
+          // Create a new blob from the array when recording stops and save it to IndexedDB
           const blob = new Blob(this.recordedBlobs, { type: 'video/webm' });
-          // Save the video to IndexedDB
           this.saveVideo(blob);
-          // Clear the recorded blobs for any subsequent recordings
           this.recordedBlobs = [];
+
           // Set the recorded video's source to the blob for playback
-          const url = URL.createObjectURL(blob);
-          this.recordedVideoElement.nativeElement.src = url;
+          this.recordedVideoElement.nativeElement.src = URL.createObjectURL(blob);
         };
   
-        // Start recording
+        // Use the MediaRecorder instance to start recording the stream and change the recording state variable
         this.mediaRecorder.start();
-        // Change the recording state
         this.isRecording = true;
       })
       .catch(err => {
+        // Print errors
         alert('Error: ' + err);
       });
   }
   
-  
+  // Method to stop recording and change the recording state variable
   stopRecording() {
     if (this.mediaRecorder) {
       this.mediaRecorder.stop();
-      this.mediaRecorder.onstop = () => {
-        const blob = new Blob(this.recordedBlobs, { type: 'video/webm' });  // Combine the recorded data into a single Blob
-        this.saveVideo(blob);  // Save the recorded video to IndexedDB
-      };
-      this.isRecording = false; // Update the recording state
+      this.isRecording = false;
     }
   }
 
-  saveVideo(blob: Blob) {  // New method to save the recorded video to IndexedDB
+  // Method to save the recorded video and display it
+  saveVideo(blob: Blob) {
+    
+    // Open the IndexedDB database and create an object store to save the video
     const request = window.indexedDB.open('RecordedVideos', 1);
     request.onupgradeneeded = (event: IDBVersionChangeEvent) => {
       const db = (event.target as IDBOpenDBRequest).result;
       db.createObjectStore('videos');
     };
+
+    // Save the video to the IndexedDB and laoad int into the video element replacing the old one
     request.onsuccess = () => {
       const db = request.result;
       const transaction = db.transaction('videos', 'readwrite');
       const videosStore = transaction.objectStore('videos');
+      
+      // Save the video as 'video1' and load it when the transaction is completed
       videosStore.put(blob, 'video1');
       transaction.oncomplete = () => {
-        this.loadVideo();  // Load the recorded video from IndexedDB
+        this.loadVideo();
       };
     };
+
+    // Print errors
     request.onerror = (event: Event) => {
       console.error('Error opening IndexedDB:', event);
     };
   }
 
-  loadVideo() {  // New method to load the recorded video from IndexedDB
+  // Method to load the recorded video from IndexedDB
+  loadVideo() {
+    // Who has time to add acual video loading anyway?
     const request = window.indexedDB.open('RecordedVideos', 1);
     request.onsuccess = () => {
       const db = request.result;
       const transaction = db.transaction('videos');
       const videosStore = transaction.objectStore('videos');
       const getRequest = videosStore.get('video1');
+
+      // Load the video from the database into a blob and pass it to the video element
       getRequest.onsuccess = () => {
         const videoBlob = getRequest.result;
+        // Holy fcking crap took me a while to figure out how to use the stupid link stuff to pass the blob to the video element
         const videoUrl = URL.createObjectURL(videoBlob);
         const mediaControl = this.recordedVideoElement.nativeElement;
         mediaControl.src = videoUrl;
       };
     };
+    // Print errors
     request.onerror = (event: Event) => {
       console.error('Error opening IndexedDB:', event);
     };
