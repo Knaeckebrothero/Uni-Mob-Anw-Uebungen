2211018fa7389fb614b145216fa549d1f27ec6dd
Ghost <OverlyGenericAddress@pm.me>
2023-11-16 17:19:03 +0100

    Added filesupport

--- src/app/operating-system/operating-system.component.ts
+++ src/app/operating-system/operating-system.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+declare let window: CustomWindow;
+
+interface CustomWindow extends Window {
+  chooseFileSystemEntries: (options: any) => Promise<any>;
+}
+
 @Component({
   selector: 'app-operating-system',
   templateUrl: './operating-system.component.html',
@@ -10,12 +16,64 @@ export class OperatingSystemComponent {
   quota: number | string = 'unknown';
   percent: number | string = 'unknown';
   persisted: string = 'unknown';
+  selectedFileCount: number = 0;
+  fileInfos: Array<{ name: string; type: string; size: number; lastModified: string }> = [];
 
   constructor() {}
 
   oninit() {
     this.checkStorageEstimate();
     this.checkPersisted();
+  }
+
+  readFiles(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement.files) {
+      const files = inputElement.files;
+      this.selectedFileCount = files.length;
+      this.fileInfos = [];
+  
+      for (let i = 0; i < files.length; i++) {
+        const file = files.item(i);
+        if (file) {
+          const reader = new FileReader();
+          
+          reader.onload = () => {
+            this.fileInfos.push({
+              name: file.name,
+              type: file.type,
+              size: file.size,
+              lastModified: new Date(file.lastModified).toLocaleDateString()
+            });
+          };
+  
+          reader.readAsText(file);
+        }
+      }
+    }
+  }    
+
+  async writeFile() {
+    // Check for API support
+    if (!('chooseFileSystemEntries' in window)) {
+      alert('Native File System API not supported');
+      return;
+    }
+
+    try {
+      const handle = await window.chooseFileSystemEntries({
+        type: 'save-file',
+      });
+
+      const file = await handle.getFile();
+      const writer = await handle.createWriter();
+      await writer.write(0, 'Hello world from What Web Can Do!');
+      await writer.close();
+
+      // Update UI or handle success
+    } catch (error) {
+      // Handle errors
+    }
   }
 
   checkStorageEstimate() {
