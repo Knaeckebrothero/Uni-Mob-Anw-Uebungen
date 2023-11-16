dab6d978a3c7148763da022c1e0c2c32bd0f7cd9
Ghost <OverlyGenericAddress@pm.me>
2023-11-16 16:46:15 +0100

    Added Storage Quotas

--- src/app/operating-system/operating-system.component.ts
+++ src/app/operating-system/operating-system.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./operating-system.component.scss']
 })
 export class OperatingSystemComponent {
+  usage: number | string = 'unknown';
+  quota: number | string = 'unknown';
+  percent: number | string = 'unknown';
+  persisted: string = 'unknown';
 
+  constructor() {}
+
+  oninit() {
+    this.checkStorageEstimate();
+    this.checkPersisted();
+  }
+
+  checkStorageEstimate() {
+    if ('storage' in navigator && 'estimate' in navigator.storage) {
+      navigator.storage.estimate().then(estimate => {
+        this.usage = estimate.usage ?? 'unknown';
+        this.quota = estimate.quota ?? 'unknown';
+        this.percent = estimate.quota && estimate.usage !== undefined 
+                       ? ((estimate.usage * 100) / estimate.quota).toFixed(0) 
+                       : 'unknown';
+      });
+    }
+  }
+
+  checkPersisted() {
+    if ('storage' in navigator && 'persisted' in navigator.storage) {
+      navigator.storage.persisted().then(persisted => {
+        this.persisted = persisted ? 'persisted' : 'not persisted';
+      });
+    }
+  }
+
+  requestPersistence() {
+    if ('storage' in navigator && 'persist' in navigator.storage) {
+      navigator.storage.persist().then(persisted => {
+        this.persisted = persisted ? 'persisted' : 'not persisted';
+      });
+    }
+  }
 }
