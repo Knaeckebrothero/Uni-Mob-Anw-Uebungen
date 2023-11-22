3aceae53b8b612adc4700a4829733dca3ee1be62
Ghost <OverlyGenericAddress@pm.me>
2023-11-22 19:40:16 +0100

    Added additional permission checks and stuff

--- src/app/native-behaviors/native-behaviors.component.ts
+++ src/app/native-behaviors/native-behaviors.component.ts
@@ -1,17 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-native-behaviors',
   templateUrl: './native-behaviors.component.html',
   styleUrls: ['./native-behaviors.component.scss']
 })
-export class NativeBehaviorsComponent {
+export class NativeBehaviorsComponent implements OnInit {
   permissionStatus: NotificationPermission = 'default';
+  permissionStatuses: any = {};
 
   constructor() {
+  }
+
+  ngOnInit(): void {
+    this.checkNotificationPermission();
+    this.checkAllPermissions();
+  }
+
+  private checkNotificationPermission() {
     if ('Notification' in window) {
       this.permissionStatus = Notification.permission;
     }
+  }
+
+  private checkAllPermissions() {
+    if ('permissions' in navigator) {
+      const permissionsToCheck = ['geolocation', 'notifications', 'push', 'midi', 'camera', 'microphone', 'background-sync', 'ambient-light-sensor', 'accelerometer', 'gyroscope', 'magnetometer'];
+      for (const permission of permissionsToCheck) {
+        this.checkPermission(permission);
+      }
+    }
+  }
+
+  private checkPermission(permissionName: string, descriptor?: any) {
+    navigator.permissions.query({ name: permissionName, ...descriptor })
+      .then(permissionStatus => {
+        this.permissionStatuses[permissionName] = permissionStatus.state;
+        permissionStatus.addEventListener('change', () => {
+          this.permissionStatuses[permissionName] = permissionStatus.state;
+        });
+      })
+      .catch(error => console.error('Error checking permission:', permissionName, error));
+  }
+
+  requestGeolocation() {
+    navigator.geolocation.getCurrentPosition(() => {}, () => {}, {});
+  }
+
+  requestNotifications() {
+    Notification.requestPermission().then(result => {
+      this.permissionStatus = result;
+    });
   }
 
   requestPermission() {
@@ -48,6 +87,31 @@ export class NativeBehaviorsComponent {
       reg?.showNotification("Hi there - persistent!");
     }).catch(err => {
       alert('Service Worker registration error: ' + err);
+    });
+  }
+
+  subscribeToPush() {
+    navigator.serviceWorker.ready.then(registration => {
+      return registration.pushManager.subscribe({ userVisibleOnly: true })
+        .then(subscription => {
+          console.log('Push Subscription:', subscription);
+        })
+        .catch(error => {
+          console.error('Push Subscription error:', error);
+        });
+    });
+  }
+
+  checkPushSubscription() {
+    navigator.serviceWorker.ready.then(registration => {
+      registration.pushManager.getSubscription()
+        .then(subscription => {
+          if (subscription) {
+            console.log('Already subscribed to push:', subscription);
+          } else {
+            console.log('Not subscribed to push');
+          }
+        });
     });
   }
 }
